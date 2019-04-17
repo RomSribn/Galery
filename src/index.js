2bf678854fc515f420595bf6de6c24379651ede5
RomSribn <steeper.fest@gmail.com>
2019-04-17 16:13:52 +0300

    moving slides

--- src/index.js
+++ src/index.js
@@ -2,20 +2,54 @@ import "./style.css";
 import Game from "./images/15Game.png";
 import Maze from "./images/Maze.png";
 import Solitaire from "./images/Solitaire.png";
+import Empty from "./images/empty.png"
 
 
 const slidesNode = document.querySelectorAll(".wrapper")[0].children;
+const slidesArray = [];
 const $slidePlace = document.querySelector(".frames");
-console.log(slidesNode);
+const controls = {
+    left: document.querySelector(".control.left"),
+    right: document.querySelector(".control.right")
+};
 
-for (let i = 0; i < slidesNode.length; i ++){
-    slidesNode[i].addEventListener("click", handleClick)
+for (let i = 2; i < slidesNode.length; i ++){
+    slidesNode[i].addEventListener("click", handleOnSlideClick);
+    slidesArray.push(slidesNode[i]);
+}
+const slidesDisplayPosition = {
+    start: 0,
+    end:slidesArray.length-1
+};
+
+console.log(slidesDisplayPosition.end);
+controls.left.addEventListener("click", handleSlideLeft);
+controls.right.addEventListener("click", handleSlideRight);
+// console.log(slidesNode[2].remove());
+
+function handleSlideLeft(evt) {
+    console.log(this.dataset.control === "left");
+    if(this.dataset.control === "left"){
+        let removedNode = slidesArray[slidesDisplayPosition.start];
+        slidesArray[slidesDisplayPosition.start].hidden = true;
+        slidesArray[slidesDisplayPosition.end].hidden = false;
+        // slidesDisplayPosition.start = slidesDisplayPosition.start + 1;
+    }
 }
 
-function handleClick(evt) {
+
+function handleSlideRight(evt) {
+    if(this.dataset.control === "right"){
+        let removedNode = slidesArray[slidesDisplayPosition.end];
+        slidesArray[slidesDisplayPosition.end].hidden = true;
+        slidesArray[slidesDisplayPosition.start].hidden = false;
+        // slidesDisplayPosition.end = slidesDisplayPosition.end - 1;
+    }
+}
+
+function handleOnSlideClick(evt) {
     evt.stopPropagation();
     const target = evt.target;
-    // <iframe src="frames/15Game/index.html" width="100%"  height="800" id="15Game"></iframe>
     if(this.dataset.type === "slide"){
             $slidePlace.replaceChild(createFrame(this.dataset.slide), $slidePlace.firstChild)
     }
